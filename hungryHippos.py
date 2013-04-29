d125a7d160b38102d37311c6b180d8ceab2755cd
Brad Stevenson <BradStevenson@outlook.com>
2013-04-29 19:26:37 +0100

    Bombs included, multiple game modes, updated images

--- hungryHippos.py
+++ hungryHippos.py
@@ -4,7 +4,7 @@ import pygame
 import random
 import math
 from pygame.sprite import Sprite
-from Menu import Menu
+from menu import Menu
 
 pygame.display.init()
 pygame.font.init()
@@ -56,14 +56,12 @@ class Hippo(Sprite):
 		self.rect = pygame.Rect(self.position, (100, 100))
 		pygame.draw.rect(self.image, pygame.Color("black"), self.rect)
 		self.state = False
-		
-class Ball(Sprite):
+
+class damageBall(Sprite):
 	""" HANDLES BALL BEHAVIOURS """
-	def __init__(self, color, score, hippos):
+	def __init__(self, score, hippos):
 		Sprite.__init__(self)
-		self.image = pygame.Surface((20, 20))
-		self.image.set_colorkey(pygame.Color("black"))
-		pygame.draw.circle(self.image, pygame.Color(color), (10, 10), 10)
+		self.image = pygame.image.load('Images/Bomb.png')
 
 		self.hippos = hippos
 		self.score = score
@@ -72,28 +70,30 @@ class Ball(Sprite):
 		angle = random.uniform(0, (math.pi/2)-0.18) # 0.18 ~= 10 degress
 		angle *= random.choice([-1,1])
 		side = random.choice([-1,1])
-		self.velocity = [side * 15 * math.cos(angle), 15 * math.sin(angle)]
+		self.velocity = [side * 10 * math.cos(angle), 10 * math.sin(angle)]
 		self.start = (WIDTH_RES/2, HEIGHT_RES/2)
 
+
 	def update(self):
+		global totalScore
+		#  If ball is in play edit trajectory
 		if self.image.get_alpha() != 0:
 			self.velocity[0] += 0.01
 	 		self.velocity[1] -= 0.01
 
 		self.rect.move_ip(*self.velocity)
+
 		# Detect if eaten
 		for hippo in self.hippos:
 			if self.rect.colliderect(hippo.rect):
 				if hippo.state == True:
 					for score in self.score:
 						if score.player == hippo.player:
-							self.score[score.player-1].increase()
+							self.score[score.player-1].decrease()
 					self.rect.center = self.start
 					self.velocity = [0, 0]
-					self.image.set_alpha(0)
-					global totalScore
+					self.image = pygame.Surface((0,0))
 					totalScore += 1
-					return True
 				else:
 					self.velocity[0] *= -1
 					self.velocity[1] *= -1
@@ -114,47 +114,131 @@ class Ball(Sprite):
 		elif self.rect.right > WIDTH_RES:
 			self.velocity[0] *= -1
 			self.rect.right = WIDTH_RES-1
-		return False
+		
+class Ball(Sprite):
+	""" HANDLES BALL BEHAVIOURS """
+	def __init__(self, score, hippos):
+		Sprite.__init__(self)
+		self.image = pygame.image.load('Images/ball.png')
+
+		self.hippos = hippos
+		self.score = score
+		self.rect = self.image.get_rect()
+		self.rect.center = (WIDTH_RES/2, HEIGHT_RES/2)
+		angle = random.uniform(0, (math.pi/2)-0.18) # 0.18 ~= 10 degress
+		angle *= random.choice([-1,1])
+		side = random.choice([-1,1])
+		self.velocity = [side * 15 * math.cos(angle), 15 * math.sin(angle)]
+		self.start = (WIDTH_RES/2, HEIGHT_RES/2)
+
+
+	def update(self):
+		global totalScore
+		#  If ball is in play edit trajectory
+		if self.image.get_alpha() != 0:
+			self.velocity[0] += 0.01
+	 		self.velocity[1] -= 0.01
+
+		self.rect.move_ip(*self.velocity)
+
+		# Detect if eaten
+		for hippo in self.hippos:
+			if self.rect.colliderect(hippo.rect):
+				if hippo.state == True:
+					for score in self.score:
+						if score.player == hippo.player:
+							self.score[score.player-1].increase()
+					self.rect.center = self.start
+					self.velocity = [0, 0]
+					self.image = pygame.Surface((0,0))
+					totalScore += 1
+				else:
+					self.velocity[0] *= -1
+					self.velocity[1] *= -1
+
+		# bounce ball of the top screen border
+		if self.rect.top < 0:
+			self.velocity[1] *= -1
+			self.rect.top = 1
+		# bounce ball off bottom screen border
+		elif self.rect.bottom > HEIGHT_RES:
+			self.velocity[1] *= -1
+			self.rect.bottom = HEIGHT_RES-1
+		# bounce ball off left screen border
+		elif self.rect.left < 0:
+			self.velocity[0] *= -1
+			self.rect.left = 1
+		# bounce ball off right screen border
+		elif self.rect.right > WIDTH_RES:
+			self.velocity[0] *= -1
+			self.rect.right = WIDTH_RES-1
 
 class Score(Sprite):
-	def __init__(self, color, position, player):
+	def __init__(self, color, position, player, text):
 		pygame.sprite.Sprite.__init__(self)
 
 		self.color = pygame.Color(color)
-		if(player <= 2):
-			self.size = (20,300)
-		else:
-			self.size = (300, 20)
-
-		self.image = pygame.Surface(self.size)
-		self.image.set_colorkey(pygame.Color("black"))
+		self.text = text
 		self.player = player
+		self.position = position
 		self.score = 0
-		self.rect = pygame.Rect(position, self.size)
-		self.render_balls()
+
+		if self.text:
+			self.font = pygame.font.Font("pixelated_bold.ttf", 36)
+			self.render_text()
+			self.rect = self.image.get_rect()
+
+		else:
+			if self.player <= 2:
+				self.size = (20,300)
+			else:
+				self.size = (300, 20)
+			self.image = pygame.Surface(self.size)
+			self.image.set_colorkey(pygame.Color("black"))
+			self.rect = pygame.Rect(position, self.size)
+			self.render_balls()
+
+		self.rect.center = position
 
 	def render_balls(self):
+		self.image = pygame.Surface(self.size)
+		self.image.set_colorkey(pygame.Color("black"))
+		self.rect = pygame.Rect(self.position, self.size)
 		for i in range (0, self.score):
-			if(self.player <= 2):
+			if self.player <= 2:
 				pygame.draw.circle(self.image, self.color, (10, (i*20)+10), 10)
 			else:
 				pygame.draw.circle(self.image, self.color, ((i*20)+10, 10), 10)
 
+	def render_text(self):
+		self.image = self.font.render(str(self.score), True, self.color)
+
 	def increase(self):
 		self.score += 1
-		self.render_balls()
-		
-def main():
+		if self.text:
+			self.render_text()
+		else:
+			self.render_balls()
 
-	def createBall(players):
-		if players == 2:
-			return Ball("white", [score1, score2], [player1, player2])
-		elif players == 3:
-			return Ball("white", [score1, score2, score3], [player1, player2, player3])
-		elif players == 4:
-			return Ball("white", [score1, score2, score3, score4], [player1, player2, player3, player4])
+	def decrease(self):
+		self.score -= 2
+		if self.text:
+			self.render_text()
+		else:
+			self.render_balls()
 
-	def getWinner(scores):
+def setScreen(screen, background):
+	screen = pygame.display.set_mode((WIDTH_RES, HEIGHT_RES))
+	background = pygame.image.load('Images/background.png')
+	screen.blit(background,(0, 0))
+
+def createBall(scoreList, playerList):
+	if random.randint(1,10) == 1:
+		return damageBall(scoreList, playerList)
+	else:
+		return Ball(scoreList, playerList)
+
+def getWinner(scores):
 		highScore = 0
 		winner = 0
 		for score in scores:
@@ -163,35 +247,38 @@ def main():
 				winner = score.player
 		return winner
 
-	def setScreen():
-		screen = pygame.display.set_mode((WIDTH_RES, HEIGHT_RES))
-		background = pygame.image.load('Images/background.png')
-		screen.blit(background,(0, 0))
-
-	setScreen()
+def main():
 
 	global totalScore
 	players = 0
-	totalScore = 0
 	counter = 0
 	clock = pygame.time.Clock()
-	start_menu = True
+	screen = pygame.display.set_mode((WIDTH_RES, HEIGHT_RES))
+	background = pygame.image.load('Images/background.png')
+	game_menu = True
+	player_menu = True
 	running_game = True
 	win_menu = True
+	apocalypse = False
+	multiplayer = False
+	singleplayer = False
+
+	# Game type menu
+	setScreen(screen, background)
 
 	menu = Menu()
-	menu.init([' HUNGRY HIPPOS ', '  2 player ', '  3 player ', '  4 player '], screen, True)
+	menu.init([' HUNGRY HIPPOS ', '  infinite mode ', '  multiplayer ', '  single player '], screen, True)
 	menu.draw()
 
-	# Player choice menu
-	while start_menu:
+	while game_menu:
 		clock.tick(60)
 		pygame.display.flip()
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
+				game_menu = False
+				player_menu = False
 				running_game = False
-				start_menu = False
 				win_menu = False
 			elif event.type == pygame.KEYDOWN:
 			   	if event.key == pygame.K_UP:
@@ -200,60 +287,85 @@ def main():
 				   	menu.draw(1)
 			  	elif event.key == pygame.K_RETURN:
 				   	if menu.get_position() == 1:
-				   		players = 2
+				   		apocalypse = True
 				   	elif menu.get_position() == 2:
-				   		players = 3
+				   		multiplayer = True
 				   	elif menu.get_position() == 3:
+				   		singleplayer = True
+ 				   	game_menu = False
+
+
+	# Player choice menu
+	setScreen(screen, background)
+
+	menu = Menu()
+	menu.init(['  2 player ', '  3 player ', '  4 player ', '  back '], screen, False)
+	menu.draw()
+
+	while player_menu:
+		clock.tick(60)
+		pygame.display.flip()
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running_game = False
+				player_menu = False
+				win_menu = False
+			elif event.type == pygame.KEYDOWN:
+			   	if event.key == pygame.K_UP:
+			   		menu.draw(-1)
+			  	elif event.key == pygame.K_DOWN:
+				   	menu.draw(1)
+			  	elif event.key == pygame.K_RETURN:
+				   	if menu.get_position() == 0:
+				   		players = 2
+				   	elif menu.get_position() == 1:
+				   		players = 3
+				   	elif menu.get_position() == 2:
 				   		players = 4
-				   	start_menu = False
+				   	elif menu.get_position() == 3:
+				   		main()
+				   	player_menu = False
 
 	# Reset Screen to start game
-	setScreen()
+	setScreen(screen, background)
 
 	# Initialize Sprites based on user chosen players.
 	if players >= 2:
-		score1 = Score("white", (20, 10), 1)
-		score2 = Score("white", ((WIDTH_RES-40, (HEIGHT_RES/2)+70)), 2)
+		score1 = Score("white", (20, 24), 1, apocalypse)
+		score2 = Score("white", ((WIDTH_RES-40, (HEIGHT_RES/2)+60)), 2, apocalypse)
 		scoreList = [score1, score2]
 		player1 = Hippo((0, (HEIGHT_RES/2)-50), 1)
 		player2 = Hippo((WIDTH_RES-100, (HEIGHT_RES/2)-50), 2)
-		sprites = pygame.sprite.RenderClear([score1, score2, player1, player2])
-		# KEYMAP
+		playerList = [player1, player2]
 		key_map = {
 			pygame.K_q: [player1.forward, player1.back],
 			pygame.K_p: [player2.forward, player2.back]
 		}
-	if players >= 3:
-		score3 = Score("white", ((WIDTH_RES/2)+70, 20), 3)
-		scoreList = [score1, score2, score3]
-		player3 = Hippo(((WIDTH_RES/2)-50, 0), 3)
-		sprites = pygame.sprite.RenderClear([score1, score2, score3, player1, player2, player3])
-		# KEYMAP
-		key_map = {
-			pygame.K_q: [player1.forward, player1.back],
-			pygame.K_p: [player2.forward, player2.back],
-			pygame.K_m: [player3.forward, player3.back]
-		}
-	if players == 4:
-		score4 = Score("white", (10, HEIGHT_RES-40), 4)
-		scoreList = [score1, score2, score3, score4]
-		player4 = Hippo(((WIDTH_RES/2)-50, HEIGHT_RES-100), 4)
-		sprites = pygame.sprite.RenderClear([score1, score2, score3, score4, player1, player2, player3, player4])
-		# KEYMAP
-		key_map = {
-			pygame.K_q: [player1.forward, player1.back],
-			pygame.K_p: [player2.forward, player2.back],
-			pygame.K_m: [player3.forward, player3.back],
-			pygame.K_z: [player4.forward, player4.back]
-		}
 
-	ball1 = createBall(players)
+	if players >= 3:
+		score3 = Score("white", ((WIDTH_RES/2)+70, 20), 3, apocalypse)
+		scoreList.append(score3)
+		player3 = Hippo(((WIDTH_RES/2)-50, 0), 3)
+		playerList.append(player3)
+		key_map[pygame.K_m] = [player3.forward, player3.back]
+
+	if players == 4:
+		score4 = Score("white", (10, HEIGHT_RES-40), 4, apocalypse)
+		scoreList.append(score4)
+		player4 = Hippo(((WIDTH_RES/2)-50, HEIGHT_RES-100), 4)
+		playerList.append(player4)
+		key_map[pygame.K_z] = [player4.forward, player4.back]
+
+	sprites = pygame.sprite.RenderClear(scoreList + playerList)
+	ball = createBall(scoreList, playerList)
 
 	# Set balls into a list so more can be dynamically added
-	ballList = [ball1]
+	ballList = [ball]
 	ballSprite = pygame.sprite.RenderClear(ballList)
 
 	# Run Game
+	stage = 91
 	while running_game:
 		clock.tick(60)
 		counter += 1
@@ -261,9 +373,17 @@ def main():
 		ballSprite.update()
 
 		# Every 60 seconds add a ball whilst there are less balls than 10*players
-		if (counter%60 == 0 and len(ballList) < players*10):
-			ballList.append(createBall(players))
-			ballSprite = pygame.sprite.RenderClear(ballList)
+		if apocalypse:
+			if  stage > 1 and counter%90 == 0:
+				stage -= 1
+				print stage
+			if counter%stage == 0:
+				ballList.append(createBall(scoreList, playerList))
+				ballSprite = pygame.sprite.RenderClear(ballList)
+		else:
+			if (counter%60 == 0 and len(ballList) < players*10):
+				ballList.append(createBall(scoreList, playerList))
+				ballSprite = pygame.sprite.RenderClear(ballList)
 
 		sprites.draw(screen)
 		ballSprite.draw(screen)
@@ -281,13 +401,13 @@ def main():
 				key_map[event.key][1]()
 
 		# When all balls have been used end game loop.
-		if totalScore == players*10:
+		if totalScore == players*10 and not apocalypse:
 			running_game = False
 
-	
+	# End game menu 
 	menu = Menu()
 	winner = getWinner(scoreList)
-	menu.init(['Player '+str(winner)+' Wins!', '  Play again  ', '     Quit     '], screen, True)
+	menu.init(['PLAYER '+str(winner)+' WINS!', '  play again  ', '     quit     '], screen, True)
 	menu.draw()
 
 	while win_menu:
